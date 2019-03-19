304f7facdedc0632667c1debf203dfd0e9000002
Simon Green <simon.green@thielsen.co.uk>
2019-03-19 15:38:41 +0000

    Added drawing of bats to screen

--- ping/game.py
+++ ping/game.py
@@ -1,30 +1,42 @@
 import pygame
 from pygame.locals import *
+from bat import Bat
 
 class Game:
 
-    HEIGHT = 600
-    WIDTH = 800
+    SCREEN_HEIGHT = 600
+    SCREEN_WIDTH = 800
+    COLOUR = (255, 255, 255)
+    BAT_WIDTH = 10
+    BAT_HEIGHT = 100
 
 
     def __init__(self):
 
         pygame.init()
-
-        self.screen = pygame.display.set_mode((Game.WIDTH, Game.HEIGHT))
-
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((Game.SCREEN_WIDTH, Game.SCREEN_HEIGHT))
+        self.bats = Bat(Game.SCREEN_WIDTH,
+                        Game.SCREEN_HEIGHT,
+                        Game.BAT_WIDTH,
+                        Game.BAT_HEIGHT)
 
 
     def game_loop(self):
-
-        self.running = True
 
         while self.running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         self.running = False
+        
+            self.screen.fill((0, 0, 0))
+            pygame.draw.rect(self.screen, self.COLOUR, self.bats.left_bat)
+            pygame.draw.rect(self.screen, self.COLOUR, self.bats.right_bat)
 
+            self.clock.tick(60)
+            pygame.display.flip()
 
 
 if __name__ == "__main__":
