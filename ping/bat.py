dfff6f913cd60c10a307308a9a1cc1f8b8460de7
Simon Green <simon.green@thielsen.co.uk>
2019-03-19 14:32:15 +0000

    Added move down functionality

--- ping/bat.py
+++ ping/bat.py
@@ -11,7 +11,12 @@ class Bat:
                                      bat_width,
                                      bat_height)
 
-    def move_up(self, side, screen_height, move_distance):
+    def move_up(self, side, screen_height, bat_height, move_distance):
         bat = self.left_bat if side == 'left' else self.right_bat
         if bat.y - move_distance > 0:
             bat.y -= move_distance
+
+    def move_down(self, side, screen_height, bat_height, move_distance):
+        bat = self.left_bat if side == 'left' else self.right_bat
+        if bat.y + move_distance < screen_height - bat_height:
+            bat.y += move_distance
--- tests/bat_test.py
+++ tests/bat_test.py
@@ -11,12 +11,24 @@ def test_right_bat_drawn_correctly():
 
 def test_left_bat_move_up():
     bats = Bat(1600, 800, 10, 100)
-    bats.move_up('left', 800, 10)
+    bats.move_up('left', 800, 10, 10)
     assert repr(bats.left_bat) == '<rect(0, 340, 10, 100)>'
     assert repr(bats.right_bat) == '<rect(1590, 350, 10, 100)>'
 
 def test_left_bat_move_up():
     bats = Bat(1600, 800, 10, 100)
-    bats.move_up('right', 800, 10)
+    bats.move_up('right', 800, 10, 10)
     assert repr(bats.left_bat) == '<rect(0, 350, 10, 100)>'
     assert repr(bats.right_bat) == '<rect(1590, 340, 10, 100)>'
+
+def test_left_bat_move_down():
+    bats = Bat(1600, 800, 10, 100)
+    bats.move_down('left', 800, 10, 10)
+    assert repr(bats.left_bat) == '<rect(0, 360, 10, 100)>'
+    assert repr(bats.right_bat) == '<rect(1590, 350, 10, 100)>'
+
+def test_left_bat_move_down():
+    bats = Bat(1600, 800, 10, 100)
+    bats.move_down('right', 800, 10, 10)
+    assert repr(bats.left_bat) == '<rect(0, 350, 10, 100)>'
+    assert repr(bats.right_bat) == '<rect(1590, 360, 10, 100)>'
