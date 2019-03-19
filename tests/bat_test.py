9b677f92e92ffa409677fb1f19b92b86b8404abe
Simon Green <simon.green@thielsen.co.uk>
2019-03-19 12:28:22 +0000

    Bat created at specified coordinates

--- tests/bat_test.py
+++ tests/bat_test.py
@@ -3,8 +3,8 @@ from ping.bat import Bat
 
 def test_left_bat_drawn_correctly():
     bats = Bat(1600, 800, 10, 100)
-    assert bats.left_bat == ''
+    assert repr(bats.left_bat) == '<rect(0, 350, 10, 100)>'
 
 def test_right_bat_drawn_correctly():
     bats = Bat(1600, 800, 10, 100)
-    assert bats.right_bat == ''
+    assert repr(bats.right_bat) == '<rect(1590, 350, 10, 100)>'
