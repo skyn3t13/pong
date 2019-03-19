a6d11d3271dffb3cf3eb28adcc671a362f90449a
Simon Green <simon.green@thielsen.co.uk>
2019-03-19 13:12:52 +0000

    Added move_up with tests

--- ping/bat.py
+++ ping/bat.py
@@ -10,3 +10,8 @@ class Bat:
                                      screen_height / 2 - bat_height / 2,
                                      bat_width,
                                      bat_height)
+
+    def move_up(self, side, screen_height, move_distance):
+        bat = self.left_bat if side == 'left' else self.right_bat
+        if bat.y - move_distance > 0:
+            bat.y -= move_distance
--- tests/bat_test.py
+++ tests/bat_test.py
@@ -8,3 +8,15 @@ def test_left_bat_drawn_correctly():
 def test_right_bat_drawn_correctly():
     bats = Bat(1600, 800, 10, 100)
     assert repr(bats.right_bat) == '<rect(1590, 350, 10, 100)>'
+
+def test_left_bat_move_up():
+    bats = Bat(1600, 800, 10, 100)
+    bats.move_up('left', 800, 10)
+    assert repr(bats.left_bat) == '<rect(0, 340, 10, 100)>'
+    assert repr(bats.right_bat) == '<rect(1590, 350, 10, 100)>'
+
+def test_left_bat_move_up():
+    bats = Bat(1600, 800, 10, 100)
+    bats.move_up('right', 800, 10)
+    assert repr(bats.left_bat) == '<rect(0, 350, 10, 100)>'
+    assert repr(bats.right_bat) == '<rect(1590, 340, 10, 100)>'
