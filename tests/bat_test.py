0385787cd1bd80dce83d2b49df80d0225c2ede09
Simon Green <simon.green@thielsen.co.uk>
2019-03-20 12:06:37 +0000

    added further tests

--- tests/bat_test.py
+++ tests/bat_test.py
@@ -17,3 +17,17 @@ def test_bat_move_down():
     bat = Bat(1600, 10, 100, 400, 400)
     bat.move_down(10)
     assert repr(bat.rect) == '<rect(400, 410, 10, 100)>'
+
+
+def test_bat_wont_move_down_when_at_edge():
+    bat = Bat(1600, 10, 100, 0, 1590)
+    print(bat.rect)
+    bat.move_down(10)
+    print(bat.rect)
+    assert repr(bat.rect) == '<rect(0, 1590, 10, 100)>'
+
+
+def test_bat_wont_move_up_when_at_edge():
+    bat = Bat(1600, 10, 100, 0, 10)
+    bat.move_up(10)
+    assert repr(bat.rect) == '<rect(0, 10, 10, 100)>'
