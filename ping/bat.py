daf52027852f7c1a65c206f6246cc1a0b6c5b24a
Simon Green <simon.green@thielsen.co.uk>
2019-03-20 11:51:15 +0000

    refactor

--- ping/bat.py
+++ ping/bat.py
@@ -2,13 +2,12 @@ import pygame
 
 
 class Bat(pygame.sprite.Sprite):
-    def __init__(self, screen_width, screen_height, bat_width, bat_height, x, y):
+    def __init__(self, screen_height, bat_width, bat_height, x, y):
         self.surf = pygame.Surface((bat_width, bat_height))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.screen_width = screen_width
         self.screen_height = screen_height
         self.bat_width = bat_width
         self.bat_height = bat_height
--- tests/bat_test.py
+++ tests/bat_test.py
@@ -3,22 +3,17 @@ from ping.bat import Bat
 
 
 def test_bat_drawn_correctly():
-    bat = Bat(1600, 800, 10, 100, 0, 800)
+    bat = Bat(1600, 10, 100, 0, 800)
     assert repr(bat.rect) == '<rect(0, 800, 10, 100)>'
 
 
 def test_bat_move_up():
-    bat = Bat(1600, 800, 10, 100, 0, 800)
-    print(bat.rect)
+    bat = Bat(1600, 10, 100, 0, 800)
     bat.move_up(10)
-    print(bat.rect)
     assert repr(bat.rect) == '<rect(0, 790, 10, 100)>'
 
 
 def test_bat_move_down():
-    bat = Bat(1600, 800, 10, 100, 400, 400)
-    print(bat.rect)
+    bat = Bat(1600, 10, 100, 400, 400)
     bat.move_down(10)
-    print(bat.rect)
     assert repr(bat.rect) == '<rect(400, 410, 10, 100)>'
-
