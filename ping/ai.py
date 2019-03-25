a8c1f8e58fff52373be589fbbd48346131471db7
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-03-25 10:39:47 +0000

    Linting ai.py

--- ping/ai.py
+++ ping/ai.py
@@ -1,8 +1,10 @@
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import RMSprop
 import random
 import numpy as np
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.optimizers import RMSprop
+
 
 class Ai:
 
@@ -30,37 +32,37 @@ class Ai:
         # If greater we use the qval to decide the action
         # Epsilon stops decreasing at 0.1 so there is always chance of a random action
         # Te random chance means novel action can still happen
-        if (random.random() < epsilon):
+        if random.random() < epsilon:
             self.action = np.random.randint(0, 2)
         else:
             self.action = (np.argmax(self.qval))
         # Take an action now based on the above
         self.take_action(self.action)
-        
+
 
     def update_state(self, state):
         # Now we check the reward for our action
         reward = self.game.get_reward()
         # Lets find the maximum qvalue that was in the array
-        maxQ = np.max(self.qval)
+        max_q = np.max(self.qval)
         # If no reward (no win or lose this cycle)
         # Set the update to be based on the maximum Q value (a decreased value)
         if reward == 0:
-            update = reward + (self.gamma * maxQ)
+            update = reward + (self.gamma * max_q)
             self.game_over = 0
         # If not the reward itself is the update
         else:
             update = reward
             self.game_over = 1
         # create an empty numpy array y
-        y = np.zeros((1,2))
+        y_val = np.zeros((1, 2))
         # make y a copy of qval (original model output)
-        y[:] = self.qval[:]
+        y_val[:] = self.qval[:]
         # update the up or down value of the qval with the update value calculated above
         # Makes the qval based on the reward
-        y[0][self.action] = update
+        y_val[0][self.action] = update
         # Update the model based on this
-        self.model.fit(state.reshape(1, 4), y, batch_size=1, nb_epoch=1, verbose=1)
+        self.model.fit(state.reshape(1, 4), y_val, batch_size=1, nb_epoch=1, verbose=1)
         print(f"Qval is {self.qval}")
 
     def take_action(self, action):
